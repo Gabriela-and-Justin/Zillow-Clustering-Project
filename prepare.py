fe3956dd255558530d9b92af031decd5d0139d52
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-04-01 15:36:25 -0500

    prepare functions added

---
+++ prepare.py
@@ -0,0 +1,60 @@
+def get_zillow(sql):
+    url = get_db_url('zillow')
+    zillow_df = pd.read_sql(sql, url, index_col='id')
+    return zillow_df
+
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70):
+	#function that will drop rows or columns based on the percent of values that are missing:\
+	#handle_missing_values(df, prop_required_column, prop_required_row
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
+
+
+def remove_columns(df, cols_to_remove):  
+	#remove columns not needed
+    df = df.drop(columns=cols_to_remove)
+    return df
+
+def wrangle_zillow():
+    df = pd.read_csv('zillow.csv')
+    
+    # Restrict df to only properties that meet single use criteria
+    single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
+    df = df[df.propertylandusetypeid.isin(single_use)]
+    
+    # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
+    df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
+            & (df.calculatedfinishedsquarefeet>350)]
+
+    # Handle missing values i.e. drop columns and rows based on a threshold
+    df = handle_missing_values(df)
+    
+    # Add column for counties
+    df['county'] = np.where(df.fips == 6037, 'Los_Angeles',
+                           np.where(df.fips == 6059, 'Orange', 
+                                   'Ventura'))
+    
+    # drop unnecessary columns
+    df = remove_columns(df, ['id',
+       'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'heatingorsystemtypeid'
+       ,'propertycountylandusecode', 'propertylandusetypeid','propertyzoningdesc', 
+        'censustractandblock', 'propertylandusedesc'])
+    
+    # replace nulls in unitcnt with 1
+    df.unitcnt.fillna(1, inplace = True)
+    
+    # assume that since this is Southern CA, null means 'None' for heating system
+    df.heatingorsystemdesc.fillna('None', inplace = True)
+    
+    # replace nulls with median values for select columns
+    df.lotsizesquarefeet.fillna(7313, inplace = True)
+    df.buildingqualitytypeid.fillna(6.0, inplace = True)
+    
+    # Just to be sure we caught all nulls, drop them here
+    df = df.dropna()
+    
+    return df
