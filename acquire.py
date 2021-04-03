8812f2bf74119f8d5c7608c6040dff5cf9d182bc
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-04-03 14:17:25 -0500

    updating sql query

--- acquire.py
+++ acquire.py
@@ -20,33 +20,21 @@ def new_zillow_data():
     This function reads the Zillow data from the CodeUp db into a df.
     '''
     sql_query = '''
-          SELECT prop.*, 
-                pred.logerror, 
-                pred.transactiondate, 
-                air.airconditioningdesc, 
-                arch.architecturalstyledesc, 
-                build.buildingclassdesc, 
-                heat.heatingorsystemdesc, 
-                landuse.propertylandusedesc, 
-                story.storydesc, 
-                construct.typeconstructiondesc 
-
-            FROM   properties_2017 prop  
-            INNER JOIN (SELECT parcelid,
-       					  logerror,
-                          Max(transactiondate) transactiondate 
-                        FROM   predictions_2017 
-                        GROUP  BY parcelid, logerror) pred
-                    USING (parcelid) 
-                LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
-                LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
-                LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
-                LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
-                LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
-                LEFT JOIN storytype story USING (storytypeid) 
-                LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
-                    WHERE  prop.latitude IS NOT NULL 
-                        AND prop.longitude IS NOT NULL
+        select *
+        from properties_2017
+        join (
+			select parcelid, max(logerror) as logerror, max(transactiondate) as transactiondate
+			from predictions_2017
+			group by parcelid
+		    ) as pred_17
+	    using(parcelid)
+        left join airconditioningtype using(airconditioningtypeid)
+        left join architecturalstyletype using(architecturalstyletypeid)
+        left join buildingclasstype using(buildingclasstypeid)
+        left join heatingorsystemtype using(heatingorsystemtypeid)
+        left join storytype using(storytypeid)
+        left join typeconstructiontype using(typeconstructiontypeid)
+        where year(transactiondate) = 2017;
                 '''
     df = pd.read_sql(sql_query, get_connection('zillow'))
 
