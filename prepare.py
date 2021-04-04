80dff2f9e0e6bdc03473ecfdd4630d757001781d
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-04-04 13:24:43 -0500

    debugged issue where wrangle_zillow fx was only returning one county

--- prepare.py
+++ prepare.py
@@ -120,7 +120,7 @@ def wrangle_zillow():
     df = remove_columns(df, ['id',
        'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'heatingorsystemtypeid'
        ,'propertycountylandusecode', 'propertylandusetypeid', 
-        'censustractandblock','assessmentyear', 'unitcnt', 'Unnamed: 0'])
+        'censustractandblock','assessmentyear', 'unitcnt', 'Unnamed: 0', 'propertyzoningdesc'])
     
     # assume that since this is Southern CA, null means 'None' for heating system
     df.heatingorsystemdesc.fillna('None', inplace = True)
@@ -146,6 +146,85 @@ def wrangle_zillow():
 
     #Drop redudnant columns after creating new features
     df = remove_columns(df, ['heatingorsystemdesc', 'transactiondate', 'month_sold',
-                            'yearbuilt', 'taxamount', 'taxvaluedollarcnt','lotsizesquarefeet', 'fips'])
+                            'yearbuilt', 'taxamount', 'taxvaluedollarcnt','lotsizesquarefeet', 'fips', 'roomcnt'])
 
     return df
+
+def wrangle_zillow_2nd():
+    '''
+    This function wrangles zillow in the same ways that the original wrangle_zillow
+    does but goes further in a few key aspects based on the exploration of the 
+    zillow data:
+    - Outliers were discoverd in the following variables and removed:
+        - Taxrate: Tax Rates above 5%
+        - Acres: Acreage above 12 (~3 STDS)
+        - Squarefeet: Properties above 10,000 squareft
+    - roomcnt was dropped as a feature...most were zero and this just doesn't make sense
+    - Zipcode: 10 wrong zipcodes. For now filling with the most common corresonding zip
+    based on lat and long: 91010
+    '''
+    df = pd.read_csv('zillow.csv')
+    
+    # Restrict df to only properties that meet single use criteria
+    single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
+    df = df[df.propertylandusetypeid.isin(single_use)]
+    
+    # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
+    df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
+            & (df.calculatedfinishedsquarefeet>350)]
+
+    # Handle missing values i.e. drop columns and rows based on a threshold
+    df = handle_missing_values(df)
+    
+    # Get Dummies for Counties
+    df = get_counties(df)
+
+    # Reset indext to parcel_id
+    df.set_index('parcelid', inplace=True)
+    
+    # drop unnecessary columns
+    df = remove_columns(df, ['id',
+       'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'heatingorsystemtypeid'
+       ,'propertycountylandusecode', 'propertylandusetypeid', 
+        'censustractandblock','assessmentyear', 'unitcnt', 'Unnamed: 0', 'propertyzoningdesc'])
+    
+    # assume that since this is Southern CA, null means 'None' for heating system
+    df.heatingorsystemdesc.fillna('None', inplace = True)
+    
+    # replace nulls with median values for select columns
+    df.lotsizesquarefeet.fillna(7313, inplace = True)
+    df.buildingqualitytypeid.fillna(6.0, inplace = True)
+    
+    # format latitude correctly using get_latitude fx
+    df = get_latitude(df)
+
+    # format longitude correctly using get_longitudefx
+    df = get_longitude(df)
+
+    #Expand Transactiondate
+    df = expand_transactiondate(df)
+
+    #Add in new features created
+    df = create_features(df)
+
+    # Just to be sure we caught all nulls, drop them here
+    df = df.dropna()
+
+    #Drop redudnant columns after creating new features
+    df = remove_columns(df, ['heatingorsystemdesc', 'transactiondate', 'month_sold',
+                            'yearbuilt', 'taxamount', 'taxvaluedollarcnt','lotsizesquarefeet', 'fips', 'roomcnt'])
+
+    #Remove Outliers
+    df = remove_outliers(df)
+
+    return df
+
+def remove_outliers(df):
+    '''
+    remove outliers in square feet, acres & tax rate
+    '''
+
+    return df[((df.acres < 13) &
+               (df.calculatedfinishedsquarefeet < 10000) & 
+               (df.taxrate < 6)
+              )]
